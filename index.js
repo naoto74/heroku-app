17a972bc3360089fcc2b8b0ec94f6f718f0aaa64
naoto74 <106652928+naoto74@users.noreply.github.com>
2022-07-03 03:19:53 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer((request, response) => {
   response.writeHead(200);
   response.write('Hello!');
